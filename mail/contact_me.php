8b5239c539979f76000994cdda40f96c7d477718
lukasz.witaszek <lukasz.witaszek@gmail.com>
2019-01-21 21:42:14 +0100

    mail-contact-me.php poprawione formatowanie pol wiadomosci

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,10 +17,14 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'kontakt@mkwadrat.witaszek.com.pl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_subject = "m2.witaszek.com.pl przesyła wiadomość:  $name";
+	$preferences = ['input-charset' => 'UTF-8', 'output-charset' => 'UTF-8'];
+	$encoded_subject = iconv_mime_encode('Subject', $email_subject, $preferences);
+	$email_subject = substr($encoded_subject, strlen('Subject: '));
+$email_body = "Nowa wiadomość ze strony m2.witaszek.com.pl\n\n"."Imię: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nWiadomość:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$headers .= "Reply-To: $email_address\n";   
+$headers .= 'MIME-Version: 1.0'.PHP_EOL.'Content-type: text/plain; charset=UTF-8';
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
